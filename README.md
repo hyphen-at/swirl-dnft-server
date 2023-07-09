b9416eb6806b63f3cd30e60870f7737c34a95edf
therne <retail3210@gmail.com>
2023-07-09 13:51:00 +0900

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Swirl dNFT SVG Renderer
+
+See production deployment here: https://swirl.deno.land/
+
+The SVG render server for Swirl. Renders SVG with given parameters. If any external images found, it fetches and embed into the SVG so it can be rendered anywhere.
+
+### Prerequisites
+
+ * Deno 1.34 or higher
+
+## Running the Server
+
+Start the server with the command:
+
+```
+deno run --allow-net main.ts
+```
+
+This starts the server at http://localhost:8000/
+
+## Deploying the Server
+
+As new commits pushed into `main` branch, Deno deploys it automatically.
+
+
+## API Documentation
+
+
+### GET `/dnft/namecard.svg`
+
+Renders Swirl Namecard image as SVG.
+
+| Name | Description | Required | Example |
+|---|---|---|---|
+| `profile_img` | (Hex-encoded) Profile Image URL | **Yes** | `68747470733a2f2f692e736..` |
+| `color` | (Hex-encoded) Card Color | **Yes** | `23366466316539` |
+| `nickname` | Nickname | **Yes** | `therne` |
+
+
+### GET `/dnft/moment.svg`
+
+Renders Swirl Moment image as SVG.
+
+| Name | Description | Required | Example |
+|---|---|---|---|
+| `profile_img` | (Hex-encoded) Profile Image URL | **Yes** | `68747470733a2f2f692e736...` |
+| `color` | (Hex-encoded) Card Color | **Yes** | `23366466316539` |
+| `nickname` | Nickname | **Yes** | `therne` |
+| `met_at` | Moment Timestamp (POSIX) | **Yes** | `1659605937` |
+
---
+++ app.ts
@@ -0,0 +1,87 @@
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import {
+  Application,
+  Router,
+  helpers,
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { encode as encodeBase64 } from "https://deno.land/std@0.194.0/encoding/base64.ts";
+import oakLogger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
+
+function decodeHex(hexString: string): string {
+  if (hexString.length % 2 !== 0) {
+    throw new Error("Invalid hexString");
+  }
+  const bytes = new Uint8Array(hexString.length / 2);
+
+  for (let i = 0; i < hexString.length; i += 2) {
+    bytes[i / 2] = parseInt(hexString.substr(i, 2), 16);
+  }
+  return new TextDecoder().decode(bytes);
+}
+
+const [nametagSvgTemplate, momentTemplate] = await Promise.all([
+  Deno.readTextFile("./assets/nametag.svg"),
+  Deno.readTextFile("./assets/moment.svg"),
+]);
+
+function renderSvgTemplate(template: string, args: Record<string, string>) {
+  let rendered = template;
+  for (const [key, value] of Object.entries(args)) {
+    rendered = rendered.replace(`{{${key}}}`, value);
+  }
+  return rendered;
+}
+
+const router = new Router();
+router.get("/", (context) => {
+  context.response.body = {
+    message: "Swirl!",
+  };
+});
+
+router.get("/dnft/nametag.svg", async (ctx) => {
+  const {
+    profile_img: profileImageUrl,
+    nickname,
+    color,
+  } = helpers.getQuery(ctx);
+  const profileImage = await fetch(decodeHex(profileImageUrl)).then((res) =>
+    res.arrayBuffer()
+  );
+
+  ctx.response.headers.set("Content-Type", "image/svg+xml");
+  ctx.response.body = renderSvgTemplate(nametagSvgTemplate, {
+    profileImageData: encodeBase64(profileImage),
+    nickname,
+    color: decodeHex(color),
+  });
+});
+
+router.get("/dnft/moment.svg", async (ctx) => {
+  const {
+    profile_img: profileImageUrl,
+    nickname,
+    color,
+    met_at: metAt,
+  } = helpers.getQuery(ctx);
+  const profileImage = await fetch(decodeHex(profileImageUrl)).then((res) =>
+    res.arrayBuffer()
+  );
+
+  ctx.response.headers.set("Content-Type", "image/svg+xml");
+  ctx.response.body = renderSvgTemplate(momentTemplate, {
+    profileImageData: encodeBase64(profileImage),
+    nickname,
+    color: decodeHex(color),
+    metAt: new Date(Number(metAt) * 1000).toLocaleDateString("en-US"),
+  });
+});
+
+const app = new Application();
+app.use(oakCors());
+app.use(oakLogger.logger);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("server running on port http://localhost:8000");
+await app.listen({ port: 8000 });
